960e3188d252bb18b4c4e51f3ab3a5dd0283712a
Anagas <anagassatheeshkumar@gmail.com>
2025-03-22 20:34:51 +0530

    Add shadcn to the project

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
+import Header from "@/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +24,23 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {/*header*/}
+          <Header />
+          <main className="min-h-screen"> {children}</main>
+          {/*footer*/}
+          <footer className="bg-muted/50 py-12">
+            <div className="container mx-auto text-center text-gray-200 px-4">
+              {" "}
+              <p>© Example Inc. All rights reserved.</p>
+            </div>
+          </footer>
+        </ThemeProvider>
       </body>
     </html>
   );
