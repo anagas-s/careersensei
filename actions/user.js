418494b9a72c5e80d4ffe5467eb2fefb89be0220
Anagas <anagassatheeshkumar@gmail.com>
2025-03-23 22:37:43 +0530

    Setup routes and build api for user onboarding

---
+++ actions/user.js
@@ -0,0 +1,94 @@
+"use server";
+import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+
+export async function updateUser(data) {
+  const { userId } = await auth();
+  if (!userId) throw new Error("Unauthorized");
+
+  const user = await db.user.findUnique({
+    where: {
+      clerkUserId: userId,
+    },
+  });
+  if (!user) throw new Error("User not found");
+
+  try {
+    const result = await db.$transaction(
+      async (tx) => {
+        //find if the industry exists
+        let industryInsight = await tx.industryInsight.findUnique({
+          where: {
+            industry: data.industry,
+          },
+        });
+        //if industry doesnt exist, create it with default values - will replace with ai later
+
+        if (!industryInsight) {
+          industryInsight = await tx.industryInsight.create({
+            data: {
+              industry: data.industry,
+              salaryRanges: [],
+              growthRate: 0,
+              demandLevel: "Medium",
+              topSkills: [],
+              marketOutlook: "Neutral",
+              keyTrends: [],
+              recommendedSkills: [],
+              nextUpdate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), //1 week
+            },
+          });
+        }
+
+        // update the user
+        const updatedUser = await tx.user.update({
+          where: {
+            id: user.id,
+          },
+          data: {
+            industry: data.industry,
+            experience: data.experience,
+            bio: data.bio,
+            skills: data.skills,
+          },
+        });
+        return { updatedUser, industryInsight };
+      },
+      {
+        timeout: 100000,
+      }
+    );
+
+    return result.user;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function getUserOnboardingStatus() {
+  const { userId } = await auth();
+  if (!userId) throw new Error("Unauthorized");
+
+  const user = await db.user.findUnique({
+    where: {
+      clerkUserId: userId,
+    },
+  });
+  if (!user) throw new Error("User not found");
+
+  try {
+    const user = await db.user.findUnique({
+      where: {
+        clerkUserId: userId,
+      },
+      select: {
+        industry: true,
+      },
+    });
+    return {
+      isOnboarded: !!user?.industry,
+    };
+  } catch (error) {
+    console.error(error.message);
+  }
+}
---
+++ app/(main)/onboarding/page.jsx
@@ -0,0 +1,15 @@
+import { industries } from "@/data/industries";
+import React from "react";
+import OnboardingForm from "./_components/onboarding-form";
+
+const OnboardingPage = () => {
+  // Check if user is already onboarded
+
+  return (
+    <main>
+      <OnboardingForm industries={industries} />
+    </main>
+  );
+};
+
+export default OnboardingPage;
